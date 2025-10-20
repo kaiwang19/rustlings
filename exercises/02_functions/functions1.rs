9cab77262db203f7fa0a5a4678a8b3d21af6d6c5
kaiwang19 <kwangbn@connect.ust.hk>
2025-10-21 01:26:03 +0800

    sec 1-5

---
+++ exercises/02_functions/functions1.rs
@@ -0,0 +1,8 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+    // Function body can be empty
+}
+
+fn main() {
+    call_me(); // Don't change this line
+}
